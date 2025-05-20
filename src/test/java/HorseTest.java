c4a9c895159bd4bf7bd146238665d9a6066f3875
Pikin <pikin@bk.ru>
2025-05-20 15:37:42 +0500

    Test Horse name isBlank added

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -20,6 +22,21 @@ public class HorseTest {
             new Horse(null, 30.0, 30.0);
         });
         assertEquals("Name cannot be null.", exception.getMessage());
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\t", "   ", "\n"})
+    public void testConstructorWhenNameIsEmptyOrWhitespace(String illegalName) {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Horse(illegalName, 30.0, 30.0);});
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\t", "   ", "\n"})
+    public void testConstructorMessageWhenNameIsEmptyOrWhitespace(String illegalName) {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Horse(illegalName, 30.0, 30.0);});
+
+        assertEquals("Name cannot be blank.", exception.getMessage());
     }
 
 }
