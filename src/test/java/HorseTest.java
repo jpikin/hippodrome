21512cb73319a95eed277d28a918b80db83c0e35
Pikin <pikin@bk.ru>
2025-05-20 15:07:07 +0500

    Test Horse class for null name added

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,2 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class HorseTest {
+
+    @Test
+    public void testConstructorWithNullName() {
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Horse(null, 30.0, 30.0);
+        });
+    }
+
+    @Test
+    public void testConstructorMessageWhenNameIsNull() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Horse(null, 30.0, 30.0);
+        });
+        assertEquals("Name cannot be null.", exception.getMessage());
+    }
+
 }
+
