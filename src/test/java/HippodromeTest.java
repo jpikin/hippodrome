3b8972b257995cfbb41ef45cef68515f3090b517
Pikin <pikin@bk.ru>
2025-05-28 14:53:57 +0500

    Test Hippodrome getWinner method added

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -60,8 +61,26 @@ public class HippodromeTest {
         for(Horse horse : mockHorses){
             verify(horse, atLeastOnce()).move();
         }
+    }
 
+    @Test
+    public void testGetWinner() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            String name = "Horse_" + i;
+            double speed = 15.0 + i*0.1;
+            horses.add(new Horse(name, speed, 30.0));
+        }
 
+        Horse testHorse = horses.stream()
+                .max(Comparator.comparing(Horse::getDistance))
+                .get();
+
+        Hippodrome hippodrome = new Hippodrome(horses);
+        Horse speedyDriver = hippodrome.getWinner();
+        assertEquals(testHorse, speedyDriver);
+
+        
     }
 
 
