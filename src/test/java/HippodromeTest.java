df9b62ed8f8d1482db51b7781234eaf9bae0f55e
Pikin <pikin@bk.ru>
2025-05-28 11:16:50 +0500

    Tests Hippodrome constructor

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,2 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class HippodromeTest {
+
+    @Test
+    public void testConstructorWhenArgumentIsNull() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Hippodrome(null);
+        });
+    }
+
+    @Test
+    public void testConstructorMessageWhenArgumentIsNull() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Hippodrome(null);
+        });
+        assertEquals("Horses cannot be null.", exception.getMessage());
+    }
+
+    @Test
+    public void testConstructorWhenArgumentIsEmpty() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+           new Hippodrome(new ArrayList<>());
+        });
+    }
+
+    @Test
+    public void testConstructorMessageWhenArgumentIsEmpty() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Hippodrome(new ArrayList<>());
+        });
+        assertEquals("Horses cannot be empty.", exception.getMessage());
+    }
+
 }
